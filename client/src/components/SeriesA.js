cfd0b6edfaa2da35f6eb7e8b855f77a7ecef1c46
joshma91 <josh.ma91@gmail.com>
2019-05-21 00:27:12 -0700

    get keys by variable instead of constants

--- client/src/components/SeriesA.js
+++ client/src/components/SeriesA.js
@@ -8,30 +8,6 @@ import AgreementPreview from "./AgreementPreview";
 import "../App.css";
 require("dotenv").config();
 
-const COMPANY = "Company";
-const COMPANY_ENTITY_STATUS = "Company Entity Status";
-const COMPANY_ENTITY_TYPE = "Company Entity Type";
-const COMPANY_JURISDICTION = "Company Jurisdiction";
-const COMPANY_SIGNATORY = "Company Signatory";
-const COMPANY_SIGNATORY_TITLE = "Company Signatory Title";
-const COMPANY_SIGNATORY_EMAIL = "Company Signatory Email";
-const COMPANY_VALUATION = "Company Valuation";
-const OFFERING_AMOUNT = "Investment Amount";
-const OTHER_AMOUNT = "Other Investment Amount";
-const SERIES_AA_CURRENCY = "Series A Currency";
-const NUMBER_OF_SHARES = "Number of Shares";
-const PRICE_PER_SHARE = "Price per share";
-const POSTMONEY_VALUATION = "Post_money valuation";
-const AVAILABLE_OPTION_POOL_PERCENTAGE = "Available Option Pool Percentage";
-const COMMON_MAJORITY_DEF = "CommonMajorityDef";
-const BOARD_SELECTION_RIGHTS = "Board Selection Rights";
-const FOUNDER_VESTING_SCHEDULE = "Founder Vesting Schedule";
-const EFFECTIVE_DATE = "Effective Date";
-const INVESTOR = "Lead Investor";
-const INVESTOR_SIGNATORY = "Lead Investor Signatory";
-const INVESTOR_SIGNATORY_TITLE = "Lead Investor Signatory Title";
-const INVESTOR_SIGNATORY_EMAIL = "Lead Investor Signatory Email";
-
 const openLawConfig = {
   server: "https://app.openlaw.io",
   templateName: "Y-COMBINATOR SERIES A TERM SHEET",
@@ -45,32 +21,6 @@ const apiClient = new APIClient(openLawConfig.server);
 class SeriesA extends React.Component {
   //initial state of variables for Assignment Template, and web3,etc
   state = {
-    // State variables for agreement
-
-    compName: null,
-    compStatus: null,
-    compType: "Limited Liability Company",
-    compJuris: "Delaware",
-    compSign: null,
-    compSignT: null,
-    compEmail: null,
-    compVal: "",
-    offAmnt: null,
-    othAmnt: null,
-    serCurr: null,
-    numShares: "",
-    prcShares: "",
-    postVal: null,
-    optionPool: null,
-    majDef: null,
-    brdRights: null,
-    founderSch: null,
-    effDate: null,
-    investName: null,
-    investSign: null,
-    investTitle: null,
-    investEmail: null,
-
     // State variables for OpenLaw
     title: "",
     template: "",
@@ -157,138 +107,26 @@ class SeriesA extends React.Component {
   };
 
   onChange = (key, value) => {
-    switch (key) {
-      case COMPANY:
-        this.setState({ compName: value });
-        break;
-      case COMPANY_ENTITY_STATUS:
-        this.setState({ compStatus: value });
-        break;
-      case COMPANY_ENTITY_TYPE:
-        this.setState({ compType: value });
-        break;
-      case COMPANY_JURISDICTION:
-        this.setState({ compJuris: value });
-        break;
-      case COMPANY_SIGNATORY:
-        this.setState({ compSign: value });
-        break;
-      case COMPANY_SIGNATORY_TITLE:
-        this.setState({ compSignT: value });
-        break;
-      case COMPANY_SIGNATORY_EMAIL:
-        this.setState({ compEmail: value });
-        break;
-      case COMPANY_VALUATION:
-        this.setState({ compVal: value });
-        break;
-      case OFFERING_AMOUNT:
-        this.setState({ offAmnt: value });
-        break;
-      case OTHER_AMOUNT:
-        this.setState({ othAmnt: value });
-        break;
-      case SERIES_AA_CURRENCY:
-        this.setState({ serCurr: value });
-        break;
-      case NUMBER_OF_SHARES:
-        this.setState({ numShares: value });
-        break;
-      case PRICE_PER_SHARE:
-        this.setState({ prcShares: value });
-        break;
-      case POSTMONEY_VALUATION:
-        this.setState({ postVal: value });
-        break;
-      case AVAILABLE_OPTION_POOL_PERCENTAGE:
-        this.setState({ optionPool: value });
-        break;
-      case COMMON_MAJORITY_DEF:
-        this.setState({ majDef: value });
-        break;
-      case BOARD_SELECTION_RIGHTS:
-        this.setState({ brdRights: value });
-        break;
-      case FOUNDER_VESTING_SCHEDULE:
-        this.setState({ founderSch: value });
-        break;
-      case EFFECTIVE_DATE:
-        this.setState({ effDate: value });
-        break;
-      case INVESTOR:
-        this.setState({ investName: value });
-        break;
-      case INVESTOR_SIGNATORY:
-        this.setState({ investSign: value });
-        break;
-      case INVESTOR_SIGNATORY_TITLE:
-        this.setState({ investTitle: value });
-        break;
-      case INVESTOR_SIGNATORY_EMAIL:
-        this.setState({ investEmail: value });
-        break;
-    }
+    console.log(this.state.parameters)
+    const parameters = key
+    ? ({
+      ...this.state.parameters,
+      [key]: value,
+    }) : (
+      this.state.parameters
+    );
     console.log("KEY:", key, "VALUE:", value);
-  };
+    this.setState({parameters})
+    }
+  
   setTemplatePreview = async () => {
-    const {
-      compName,
-      compStatus,
-      compType,
-      compJuris,
-      compSign,
-      compSignT,
-      compEmail,
-      compVal,
-      offAmnt,
-      othAmnt,
-      serCurr,
-      numShares,
-      prcShares,
-      postVal,
-      optionPool,
-      majDef,
-      brdRights,
-      founderSch,
-      effDate,
-      investName,
-      investSign,
-      investTitle,
-      investEmail,
-      compiledTemplate
-    } = this.state;
-    try {
-      const params = {
-        [COMPANY]: compName,
-        [COMPANY_ENTITY_STATUS]: compStatus,
-        [COMPANY_ENTITY_TYPE]: compType,
-        [COMPANY_JURISDICTION]: compJuris,
-        [COMPANY_SIGNATORY]: compSign,
-        [COMPANY_SIGNATORY_TITLE]: compSignT,
-        [COMPANY_SIGNATORY_EMAIL]: compEmail,
-        [COMPANY_VALUATION]: compVal,
-        [OFFERING_AMOUNT]: offAmnt,
-        [OTHER_AMOUNT]: othAmnt,
-        [SERIES_AA_CURRENCY]: serCurr,
-        [NUMBER_OF_SHARES]: numShares,
-        [PRICE_PER_SHARE]: prcShares,
-        [POSTMONEY_VALUATION]: postVal,
-        [AVAILABLE_OPTION_POOL_PERCENTAGE]: optionPool,
-        [COMMON_MAJORITY_DEF]: majDef,
-        [BOARD_SELECTION_RIGHTS]: brdRights,
-        [FOUNDER_VESTING_SCHEDULE]: founderSch,
-        [EFFECTIVE_DATE]: effDate,
-        [INVESTOR]: investName,
-        [INVESTOR_SIGNATORY]: investSign,
-        [INVESTOR_SIGNATORY_TITLE]: investTitle,
-        [INVESTOR_SIGNATORY_EMAIL]: investEmail
-      };
-      console.log(params);
+      const {parameters, compiledTemplate} = this.state
+      console.log(parameters);
 
       const executionResult = await Openlaw.execute(
         compiledTemplate.compiledTemplate,
         {},
-        params
+        parameters
       );
       const agreements = await Openlaw.getAgreements(
         executionResult.executionResult
@@ -301,73 +139,23 @@ class SeriesA extends React.Component {
       document.getElementById("preview").scrollIntoView({
         behavior: "smooth"
       });
-    } catch (error) {
-      throw error;
-    }
-  };
+    } 
+  
 
   buildOpenLawParamsObj = async (template, creatorId) => {
-    const {
-      compName,
-      compStatus,
-      compType,
-      compJuris,
-      compSign,
-      compSignT,
-      compEmail,
-      compVal,
-      offAmnt,
-      othAmnt,
-      serCurr,
-      numShares,
-      prcShares,
-      postVal,
-      optionPool,
-      majDef,
-      brdRights,
-      founderSch,
-      effDate,
-      investName,
-      investSign,
-      investTitle,
-      investEmail
-    } = this.state;
+    const { parameters, draftId } = this.state
 
     const object = {
       templateId: template.id,
       title: template.title,
       text: template.content,
-      creator: this.state.creatorId,
-      parameters: {
-        [COMPANY]: compName,
-        [COMPANY_ENTITY_STATUS]: compStatus,
-        [COMPANY_ENTITY_TYPE]: compType,
-        [COMPANY_JURISDICTION]: compJuris,
-        [COMPANY_SIGNATORY]: compSign,
-        [COMPANY_SIGNATORY_TITLE]: compSignT,
-        [COMPANY_SIGNATORY_EMAIL]: compEmail,
-        [COMPANY_VALUATION]: compVal,
-        [OFFERING_AMOUNT]: offAmnt,
-        [OTHER_AMOUNT]: othAmnt,
-        [SERIES_AA_CURRENCY]: serCurr,
-        [NUMBER_OF_SHARES]: numShares,
-        [PRICE_PER_SHARE]: prcShares,
-        [POSTMONEY_VALUATION]: postVal,
-        [AVAILABLE_OPTION_POOL_PERCENTAGE]: optionPool,
-        [COMMON_MAJORITY_DEF]: majDef,
-        [BOARD_SELECTION_RIGHTS]: brdRights,
-        [FOUNDER_VESTING_SCHEDULE]: founderSch,
-        [EFFECTIVE_DATE]: effDate,
-        [INVESTOR]: investName,
-        [INVESTOR_SIGNATORY]: investSign,
-        [INVESTOR_SIGNATORY_TITLE]: investTitle,
-        [INVESTOR_SIGNATORY_EMAIL]: investEmail
-      },
+      creator: creatorId,
+      parameters,
       overriddenParagraphs: {},
       agreements: {},
       readonlyEmails: [],
       editEmails: [],
-      draftId: this.state.draftId
+      draftId
     };
     return object;
   };
